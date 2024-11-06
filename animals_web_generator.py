13142d52762bdc4a4a2fd66220d23003e6a489a6
hao-to <th.nguyen77@gmail.com>
2024-11-06 15:10:36 +0100

    added missing docstring

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,14 +1,26 @@
 import json
 
+
 def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
     return json.load(handle)
 
+
 animals_data = load_data('animals_data.json')
 
 
 def get_animal_data():
+
+  """
+  Prints details about each animal from the loaded JSON data.
+
+  For each animal, displays name, diet, primary location, and type if available.
+  Data is retrieved from the "characteristics" dictionary and "locations" list.
+
+  Each animal's details are separated by a blank line for readability.
+  """
+
   for animal in animals_data:
     name = animal.get("name")
     characteristics = animal.get("characteristics", {})  # extracting info from characteristics-Dict

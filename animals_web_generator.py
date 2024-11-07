b2674611d743df7b841d79f72eeec0f785febee7
hao-to <th.nguyen77@gmail.com>
2024-11-07 13:51:26 +0100

    serialization for each animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -19,44 +19,50 @@ def save_text_to_file(output_path, content):
         file.write(content)
 
 
+def serialize_animal(animal):
+    """generates html string for a single animal entry"""
+
+    output = '<li class="cards__item">\n'
+
+    # add name as title
+    name = animal.get("name")
+    if name:
+        output += f'<div class="card__title"> {name}</div>\n'
+
+    # add other characteristics as text
+    characteristics = animal.get("characteristics", {})
+    diet = characteristics.get("diet")
+    locations = animal.get("locations")
+    animal_type = characteristics.get("type")
+
+    output += '<p class="card__text">\n'
+
+    if diet:
+        output += f'<strong>Diet:</strong> {diet}<br/>\n'
+    if locations:
+        output += f'<strong>Location:</strong> {locations[0]}<br/>\n'
+    if animal_type:
+        output += f'<strong>Type: </strong>{animal_type}<br/>\n'
+    output += '  </p>\n'
+
+    # close the list item for this animal
+    output += '</li>\n'
+
+    return output
+
+
 def generate_animal_html(data):
-    """loads html template, generates/inserts animal data as html and saves result in new HTML file."""
+    """loads html template, serializes each animal to html and saves result in new HTML file."""
 
     # load html template using load_file_content function
     html_template = load_file_content('animals_template.html')
 
-    # create html string for animal data to put in placeholder
+    # generate html code for each animal and add it to list
     output = ""
     for animal in data:
-        # start a new list item with the appropriate class
-        output += '<li class="cards__item">\n'
+        output += serialize_animal(animal)
 
-        name = animal.get("name")
-        characteristics = animal.get("characteristics", {})
-        diet = characteristics.get("diet")
-        locations = animal.get("locations")
-        animal_type = characteristics.get("type")
-
-        # add name as card title
-        if name:
-            output += f'<div class="card__title"> {name}</div>\n'
-
-        # add other characteristics in card text
-        output += '<p class="card__text">\n'
-
-        if diet:
-            output += f'<strong>Diet:</strong> {diet}<br/>\n'
-        if locations:
-            output += f'<strong>Location:</strong> {locations[0]}<br/>\n'
-        if animal_type:
-            output += f'<strong>Type: </strong>{animal_type}<br/>\n'
-        output += '  </p>\n'
-
-        # close the list item for this animal
-        output += '</li>\n'
-
-    # replace placeholder in HTML template with new string (output)
-    print(output)
+    # replace placeholder in html template with generated html
     html_content = html_template.replace('__REPLACE_ANIMALS_INFO__', output)
 
     # save generated HTML content to the specified output file
@@ -65,13 +71,12 @@ def generate_animal_html(data):
 
 def main():
     """Main function to load data, generate HTML, and save output."""
-    # Load animal data from JSON file
+    # load animal data from JSON file
     animals_data = load_json_data('animals_data.json')
 
-    # Generate HTML and save it to a file
+    # generate html and save it to a file
     generate_animal_html(animals_data)
 
 
-# Run the main function only if this script is executed directly
 if __name__ == "__main__":
     main()
